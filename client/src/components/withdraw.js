aac3d5ce172adbdd0602b715fb6bbcb42c5e7e71
Samuel Crow <KungFuSambo@gmail.com>
2021-10-07 16:09:46 -0500

    Changed Withdraw

--- client/src/components/withdraw.js
+++ client/src/components/withdraw.js
@@ -1,80 +1,109 @@
 import React from "react";
 import Card from "./context";
 
-function Withdraw(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');  
+function Withdraw() {
+  const [show, setShow] = React.useState(true);
+  const [wrong, setWrong] = React.useState(false);
+  const [status, setStatus] = React.useState("");
+  let currentUser = React.useContext(UserContext);
+  const [activeUser, setActiveUser] = React.useState(currentUser.user);
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [balance, setBalance] = React.useState(null);
 
   return (
-    <Card
-      bgcolor="success"
-      header="Withdraw"
-      status={status}
-      body={show ? 
-        <WithdrawForm setShow={setShow} setStatus={setStatus}/> :
-        <WithdrawMsg setShow={setShow} setStatus={setStatus}/>}
-    />
-  )
+      <Card
+          bgcolor="primary"
+          header="Withdraw"
+          activeUser={activeUser}
+          status={status}
+          body={
+              wrong ? (
+                  <HandleWrong
+                      setShow={setShow}
+                      setWrong={setWrong}
+                      message={"Email or Amount Unknown"}
+                      return={"Return to Withdraw"}
+                  />
+              ) : show ? (
+                  <WithdrawForm
+                      setShow={setShow}
+                      setWrong={setWrong}
+                      activeUser={activeUser}
+                      setLoggedIn={setLoggedIn}
+                      loggedIn={loggedIn}
+                      setBalance={setBalance}
+                  />
+              ) : (
+                  <HandleMsg
+                      setShow={setShow}
+                      message={"Success!"}
+                      balance={balance}
+                      return={"return to Withdraw"}
+                  />
+              )
+          }
+      />
+  );
 }
 
-function WithdrawMsg(props){
-  return(<>
-    <h5>Success</h5>
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={() => {
-        props.setShow(true);
-        props.setStatus('');
-      }}>
-        Withdraw again
-    </button>
-  </>);
-}
-
-function WithdrawForm(props){
-  const [email, setEmail]   = React.useState('');
-  const [amount, setAmount] = React.useState('');
-
-  function handle(){
-    fetch(`/account/update/${email}/-${amount}`)
-    .then(response => response.text())
-    .then(text => {
-        try {
-            const data = JSON.parse(text);
-            props.setStatus(JSON.stringify(data.value));
-            props.setShow(false);
-            console.log('JSON:', data);
-        } catch(err) {
-            props.setStatus('Deposit failed')
-            console.log('err:', text);
-        }
-    });
+function WithdrawForm(props) {
+  const [amount, setAmount] = React.useState(0);
+  let currentUser = React.useContext(UserContext);
+  const email = currentUser.user;
+  if (props.activeUser !== "Not Logged In") {
+      props.setLoggedIn(true);
   }
 
+  function handle() {
+      console.log(email, amount);
+      const url = `/account/withdraw/${email}/${amount}`;
+      (async () => {
+          var res = await fetch(url);
+          var data = await res.json();
+          console.log(data);
+          props.setBalance(data.value.balance)
+          currentUser.balance = data.value.balance;
+          console.log(currentUser.balance);
+      })();
+      props.setShow(false);
+  }
 
-  return(<>
-
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} 
-      onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    Amount<br/>
-    <input type="number" 
-      className="form-control" 
-      placeholder="Enter amount" 
-      value={amount} 
-      onChange={e => setAmount(e.currentTarget.value)}/><br/>
-
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={handle}>
-        Withdraw
-    </button>
-
-  </>);
+  return (
+      <>
+          Amount
+          <br />
+          <input
+              type="number"
+              className="form-control"
+              placeholder="Enter Amount"
+              value={amount}
+              onChange={(e) => setAmount(e.currentTarget.value)}
+          />
+          <br />
+          {props.loggedIn ? (
+              <button
+                  type="submit"
+                  className="btn btn-light"
+                  onClick={handle}
+              >
+                  Withdraw
+              </button>
+          ) : (
+              <>
+                  <button
+                      type="submit"
+                      className="btn btn-light"
+                      onClick={handle}
+                      disabled
+                  >
+                      Withdraw
+                  </button>
+                  <br />
+                  Login to use the Withdraw Feature
+              </>
+          )}
+      </>
+  );
 }
 
 export default Withdraw
