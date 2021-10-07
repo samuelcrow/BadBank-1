f58a4becddc181fb5986bf04a49505c1f34d7285
Samuel Crow <KungFuSambo@gmail.com>
2021-10-07 15:02:14 -0500

    Nothing

--- client/src/components/createaccount.js
+++ client/src/components/createaccount.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "firebase/compat/auth";
+import firebase from "firebase/compat/app";
+import {UserContext} from './context';
 
 function Card(props){
   function classes(){
@@ -52,7 +54,7 @@ function CreateForm(props){
   const [name, setName]         = React.useState('');
   const [email, setEmail]       = React.useState('');
   const [password, setPassword] = React.useState('');
-  const currentUser = React.useContext(UserContext);
+  const currentUser = UserContext;
 
   
 
