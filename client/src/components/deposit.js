b7caa94ab8a877245833de9619379b52e82a2c0b
Samuel Crow <KungFuSambo@gmail.com>
2021-10-07 16:17:09 -0500

    Deposit update

--- client/src/components/deposit.js
+++ client/src/components/deposit.js
@@ -1,75 +1,120 @@
 import React from "react";
 import Card from "./context";
+import {UserContext} from "./context";
+import HandleMsg from "./context";
+import HandleWrong from "./context";
 
-function Deposit(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');  
+function Deposit() {
+  const [show, setShow] = React.useState(true);
+  const [wrong, setWrong] = React.useState(false);
+  const [status, setStatus] = React.useState("");
+  let currentUser = React.useContext(UserContext);
+  const [activeUser, setActiveUser] = React.useState(currentUser.user);
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [balance, setBalance] = React.useState(null);
 
   return (
-    <Card
-      bgcolor="warning"
-      header="Deposit"
-      status={status}
-      body={show ? 
-        <DepositForm setShow={setShow} setStatus={setStatus}/> :
-        <DepositMsg setShow={setShow} setStatus={setStatus}/>}
-    />
-  )
+      <Card
+          bgcolor="primary"
+          header="Deposit"
+          activeUser={activeUser}
+          status={status}
+          body={
+              wrong ? (
+                  <HandleWrong
+                      setShow={setShow}
+                      setWrong={setWrong}
+                      message={"Email or Amount Unknown"}
+                      return={"Return to Deposit"}
+                  />
+              ) : show ? (
+                  <DepositForm
+                      setShow={setShow}
+                      setWrong={setWrong}
+                      setStatus={setStatus}
+                      activeUser={activeUser}
+                      setLoggedIn={setLoggedIn}
+                      loggedIn={loggedIn}
+                      setBalance={setBalance}
+                  />
+              ) : (
+                  <HandleMsg
+                      setShow={setShow}
+                      message={"Success!"}
+                      balance={balance}
+                      return={"Return to Deposit"}
+                  />
+              )
+          }
+      />
+  );
 }
 
-function DepositMsg(props){
-  return (<>
-    <h5>Success</h5>
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={() => {
-          props.setShow(true);
-          props.setStatus('');
-      }}>
-        Deposit again
-    </button>
-  </>);
-} 
-
-function DepositForm(props){
-  const [email, setEmail]   = React.useState('');
-  const [amount, setAmount] = React.useState('');
-
-  function handle(){
-    fetch(`/account/update/${email}/${amount}`)
-    .then(response => response.text())
-    .then(text => {
-        try {
-            const data = JSON.parse(text);
-            props.setStatus(JSON.stringify(data.value));
-            props.setShow(false);
-            console.log('JSON:', data);
-        } catch(err) {
-            props.setStatus('Deposit failed')
-            console.log('err:', text);
-        }
-    });
+function DepositForm(props) {
+  const [amount, setAmount] = React.useState(0);
+  let currentUser = React.useContext(UserContext);
+  const email = currentUser.user;
+  if (props.activeUser !== "Not Logged In") {
+      props.setLoggedIn(true);
   }
 
-  return(<>
+  function handle() {
+      fetch(`/account/deposit/${email}/${amount}`)
+          .then((response) => response.text())
+          .then((text) => {
+              try {
+                  const data = JSON.parse(text);
+                  props.setStatus(JSON.stringify(data.value));
+                  props.setShow(false);
+                  console.log("JSON:", data);
+                  props.setBalance(data.value.balance)
+                  currentUser.balance = data.value.balance;
+                  console.log(currentUser.balance);
+              } catch (err) {
+                  props.setStatus("Deposit failed");
+                  console.log("err:", text);
+              }
+          });
+          
+  }
 
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
-      
-    Amount<br/>
-    <input type="number" 
-      className="form-control" 
-      placeholder="Enter amount" 
-      value={amount} onChange={e => setAmount(e.currentTarget.value)}/><br/>
-
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={handle}>Deposit</button>
-
-  </>);
+  return (
+      <>
+          Amount
+          <br />
+          <input
+              type="number"
+              className="form-control"
+              placeholder="Enter Amount"
+              value={amount}
+              onChange={(e) => setAmount(e.currentTarget.value)}
+          />
+          <br />
+          {props.loggedIn ? (
+              <button
+                  type="submit"
+                  className="btn btn-light"
+                  onClick={handle}
+              >
+                  Deposit
+              </button>
+          ) : (
+              <>
+                  <button
+                      type="submit"
+                      className="btn btn-light"
+                      onClick={handle}
+                      disabled
+                  >
+                      Deposit
+                  </button>
+                  <br />
+                  Login to use the Deposit Feature
+              </>
+          )}
+      </>
+  );
 }
+
 
 export default Deposit
