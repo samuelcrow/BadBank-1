5e02152fa9c3d8ed9209aa3049f00d79d34ce620
Samuel Crow <KungFuSambo@gmail.com>
2021-10-07 13:58:39 -0500

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,99 +1,34 @@
-import React from 'react'
+import React from "react";
 import NavBar from "./components/navbar";
-import {UserContext} from './components/context'
-import {BrowserRouter, Redirect, Route} from 'react-router-dom'
-import AlertBar from './components/alertbar';
-import Login from './components/login';
-import Home from './components/home';
-import CreateAccount from './components/createaccount';
-import Deposit from './components/deposit';
-import AllData from './components/alldata';
-import Withdraw from './components/withdraw';
-import Balance from './components/balance';
+import { UserContext } from "./components/context";
+import { HashRouter, Route } from "react-router-dom";
+import Login from "./components/login";
+import Home from "./components/home";
+import CreateAccount from "./components/createaccount";
+import Deposit from "./components/deposit";
+import AllData from "./components/alldata";
+import Withdraw from "./components/withdraw";
+import Balance from "./components/balance";
 
-const useState = React.useState;
-const user_data = JSON.parse(localStorage.getItem("user_data"));
+function App() {
+    return (
+        <HashRouter>
+        <div>
+            <NavBar/>
+            <UserContext.Provider value={{user:'Not Logged In'}}>
+                <div className="container" style={{padding: "20px"}}>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/createAccount" component={CreateAccount}/>
+                    <Route path="/allData" component={AllData}/>
+                    <Route path="/deposit" component={Deposit}/>
+                    <Route path="/withdraw" component={Withdraw}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/balance" component={Balance}/>
+                </div>
+            </UserContext.Provider>
+        </div>
+        </HashRouter>
+    )
+}
 
-
-const App = () => {
-  const [successAlert, setSuccessAlert] = useState(false);
-  const [alertData, setAlertData] = useState("");
-
-  const handleAlert = (data) => {
-    setSuccessAlert(true);
-    setAlertData(data);
-  };
-
-  return (
-    <BrowserRouter>
-      <div>
-        <NavBar />
-        <UserContext.Provider
-          value={{
-            users: [
-              {
-                name: "abel",
-                email: "abel@mit.edu",
-                password: "secret",
-                balance: 100,
-              },
-            ],
-          }}
-        >
-          {user_data && user_data.token ? <AlertBar alertData={alertData} /> : null}
-          <div className="container" style={{ padding: "20px" }}>
-            <Route exact path="/" exact component={Home} />
-            <Route exact path="/CreateAccount/" component={CreateAccount} />
-            <Route
-              path="/login/"
-              render={(props) => <Login {...props} handleAlert={handleAlert} />}
-            />
-            <Route path="/deposit/" render={(props) => {
-              if(user_data && user_data.token){
-                return <Deposit {...props} /> 
-              } return <Redirect to="/login" />
-            }} />
-
-<Route
-              
-              path="/alldata/"
-              render={(props) => {
-                if (user_data && user_data.token) {
-                  return <AllData {...props} />;
-                }
-                return <Redirect to="/login" />;
-              }}
-            />
-
-            {/* only if there is a token, render the withraw component. Else, redirect to login */}
-            <Route
-              exact
-              path="/withdraw/"
-              render={(props) => {
-                if (user_data && user_data.token) {
-                  return <Withdraw {...props} />;
-                }
-                return <Redirect to="/login" />;
-              }}
-            />
-            {/* <Route path="/transactions/" component={Transactions} /> */}
-            <Route
-              path="/balance/"
-              render={(props) => {
-                if (user_data && user_data.token) {
-                  return <Balance {...props} />;
-                }
-                return <Redirect to="/login" />;
-              }}
-            />
-            
-          </div>
-        </UserContext.Provider>
-      </div>
-    </BrowserRouter>
-  );
-};
-
-export default App
-
-
+export default App;
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -7,29 +7,29 @@ function NavBar(){
   return(
 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">DarkBank</a>
+      <a className="navbar-brand" href="#">DarkBank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="/CreateAccount/">Create Account</a>
+            <a className="nav-link" href="#/CreateAccount/">Create Account</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/login/">Login</a>
+            <a className="nav-link" href="#/login/">Login</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/deposit/">Deposit</a>
+            <a className="nav-link" href="#/deposit/">Deposit</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/withdraw/">Withdraw</a>
+            <a className="nav-link" href="#/withdraw/">Withdraw</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/balance/">Balance</a>
+            <a className="nav-link" href="#/balance/">Balance</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/alldata/">AllData</a>
+            <a className="nav-link" href="#/alldata/">AllData</a>
           </li> 
           <li className="nav-item">
             <a className="nav-link" onClick={handleLogout} href="#">Log Out</a>
